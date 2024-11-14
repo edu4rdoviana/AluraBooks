953262b33abef87f5951c8564e6156d7b4d24628
edu4rdoviana <139596445+edu4rdoviana@users.noreply.github.com>
2024-11-14 12:10:37 -0300

    Terminar em casa

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Quantos paulistas o Corinthians tem ',
+    'Em que ano o Corinthians ganhou a libertadores ?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
