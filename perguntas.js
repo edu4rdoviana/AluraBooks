ae9c61a46c60efb7fe50cd0c14f194e9d1946154
edu4rdoviana <139596445+edu4rdoviana@users.noreply.github.com>
2024-11-14 22:02:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,70 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Historia',
+    'Qual o primeiro presidente do Brasi ?',
+    'marechal Manuel Deodoro da Fonseca'
+)
+
+criaCartao(
+    'Geografia',
+    'qual é a capita da Italia?',
+    'A cidade de Roma'
+)
+
+criaCartao(
+    'Eduardo',
+    'Em que data é o aniversario do Eduardo?',
+    '13 de Fevereiro'
+)
+
+criaCartao(
+    'Historia',
+    'Quando aconteceu a sengunda guerra mundial?',
+    'no ano de 1945'
+)
+
+criaCartao(
+    'Química',
+    'qual é a sigla do sal de cozinha ?',
+    'NaCL'
+)
+
+criaCartao(
+    'Geografia',
+    'qual é a capita da Inglaterra?',
+    'A cidade de Londres'
+)
+
+criaCartao(
+    'Esportes',
+    'qual é o maior medalhista de natação?',
+    'Michael Phelps'
+)
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'Linguaagem de marcação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Java Scrip?',
+    'Linguagem de Programação'
+)
+
+criaCartao(
+    'Geografia',
+    'qual é a capita da Alemanha?',
+    'A cidade de Berlin'
+)
+
+criaCartao(
+    'Esportes',
+    'qual é o maior medalhista de tenis nas olimpiadas?',
+    'Venus Williams'
+)
